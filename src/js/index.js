9cdea3ed5e1f674e80cdfc41da7785fcf6444376
Eliav <eliav.s.cohen@gmail.com>
2019-02-10 15:19:31 +0200

    L141

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,46 @@
 // import { add as a, multiply as m, ID } from './tests/searchView';
 // import * as searchView from './tests/searchView';
 // console.log(`Using imported functions! ${a(ID, 2)} and ${searchView.multiply(ID, 3)} ${str}`);
+// searchQuery('pizza');
+
+// ctrl + j to open terminal 
 
 // Global app controller
-import { searchQuery } from './models/search';
-searchQuery('pizza');
+import Search from './models/search';
+
+/** Global state of the app:
+ * - Search Object
+ * - Current Recipe Object
+ * - Shopping List Object
+ * - Liked Object
+ */
+const state = {};
+
+const controlSearch = async (e) => {
+    // 1) Get query from view
+    const query = 'Pizza';
+
+    if (query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) search for recipes
+        await state.search.getResults();
+
+        // 5) Render Recipes to UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('keydown', (e) => {
+    if (e.which === 13){
+        controlSearch();
+    }
+})
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch(e);
+});
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -1,19 +1,20 @@
-// Not so good: https://crossorigin.me/
-// Very good:   https://cors-anywhere.herokuapp.com/
-// https://www.food2fork.com/api/search
-// https://www.food2fork.com/api/get
-// 769d447afbec39f60297c0f3bc6aab14
-// Axios is already un-json-ed
-// Don't forget the limit of 500 calls per day;
-
-// For whole documentation tests2/search.js
-
-// search.js
-import axios from 'axios';
-async function getResults(query){
-    const key = '769d447afbec39f60297c0f3bc6aab14';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(result.data.recipes);
+ // Not so good: https://crossorigin.me/ // Very good:   https://cors-anywhere.herokuapp.com/ // https://www.food2fork.com/api/search // https://www.food2fork.com/api/get // 769d447afbec39f60297c0f3bc6aab14 // Axios is already un-json-ed // Don't forget the limit of 500 calls per day; // For whole documentation tests2/search.js // search.js 
+ import axios from 'axios'; // async function getResults(query){ //     const key = '769d447afbec39f60297c0f3bc6aab14'; //     const proxy = 'https://cors-anywhere.herokuapp.com/'; //     try { //         const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`); //         const recipes = result.data.recipes //         console.log(recipes); //     } catch (error) { //         alert('We\'re sorry, something got wrong with the search...'); //     } // }; // export const searchQuery = getResults;
+ export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+    
+    async getResults(){
+        const key = '769d447afbec39f60297c0f3bc6aab14';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = result.data.recipes
+            // console.log(this.result);
+        } catch (error) { 
+            alert('We\'re sorry, something got wrong with the search...');
+        }
+    }
+ 
 };
-export const searchQuery = getResults;
